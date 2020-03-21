b4aade81a670641005de9303e42c42b9dbf2d45c
Zaba505 <cakub6@gmx.com>
2020-03-20 22:35:43 -0400

    feat(cli): custom enum val directive (#31)
    * feat(cli): add new custom directive for enum values
    
    * feat(golang): check for custom enum value

--- cmd/types.go
+++ cmd/types.go
@@ -29,6 +29,26 @@ var gqlcTypes = []*ast.TypeDecl{
 			}},
 		}},
 	},
+	{
+		Spec: &ast.TypeDecl_TypeSpec{TypeSpec: &ast.TypeSpec{
+			Name: &ast.Ident{Name: "as"},
+			Type: &ast.TypeSpec_Directive{Directive: &ast.DirectiveType{
+				Locs: []*ast.DirectiveLocation{
+					{Loc: ast.DirectiveLocation_ENUM_VALUE},
+				},
+				Args: &ast.InputValueList{
+					List: []*ast.InputValue{
+						{
+							Name: &ast.Ident{Name: "value"},
+							Type: &ast.InputValue_Ident{
+								Ident: &ast.Ident{Name: "String"},
+							},
+						},
+					},
+				},
+			}},
+		}},
+	},
 }
 
 func init() {
