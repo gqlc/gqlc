b5de0246ca9949d048531c11c92280f1b1de3499
Zaba505 <cakub6@gmx.com>
2020-06-06 01:19:13 -0400

    feat(cmd): begin implementing json to idl converter

--- cmd/fetch.go
+++ cmd/fetch.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"path/filepath"
@@ -12,7 +13,6 @@ import (
 
 var introQuery = `query {
 	__schema {
-		description
 		types {
 			kind
 			name
@@ -123,18 +123,244 @@ func fetch(client *http.Client, url string) (io.ReadCloser, error) {
 	}
 
 	resp, err := client.Post(url, "application/json", &query)
-	return &converter{resp.Body}, err
+	if err != nil {
+		return nil, err
+	}
+	return newConverter(resp.Body)
 }
+
+type inputValue struct {
+	Name         string `json:"name"`
+	Description  string `json:"description"`
+	DefaultValue string `json:"defaultValue"`
+	Type         *typ   `json:"type"`
+}
+
+type field struct {
+	Name              string        `json:"name"`
+	Description       string        `json:"description"`
+	Args              []*inputValue `json:"args"`
+	Type              *typ          `json:"type"`
+	IsDeprecated      bool          `json:"isDeprecated"`
+	DeprecationReason string        `json:"deprecationReason"`
+}
+
+type enum struct {
+	Name              string `json:"name"`
+	Description       string `json:"description"`
+	IsDeprecated      bool   `json:"isDeprecated"`
+	DeprecationReason string `json:"deprecationReason"`
+}
+
+type typ struct {
+	Kind          string        `json:"kind"`
+	Name          string        `json:"name"`
+	Description   string        `json:"description"`
+	OfType        *typ          `json:"ofType"`
+	Fields        []*field      `json:"fields"`
+	Interfaces    []*typ        `json:"interfaces"`
+	PossibleTypes []*typ        `json:"possibleTypes"`
+	EnumValues    []*enum       `json:"enumValues"`
+	InputFields   []*inputValue `json:"inputFields"`
+}
+
+type directive struct {
+	Name         string        `json:"name"`
+	Description  string        `json:"description"`
+	Locations    []string      `json:"locations"`
+	IsRepeatable bool          `json:"isRepeatable"`
+	Args         []*inputValue `json:"args"`
+}
+
+type decodeTyp uint8
+
+const (
+	decodeDirs decodeTyp = iota
+	decodeTypes
+)
 
 // converter converts the GraphQL introspection response to the GraphQL IDL
 type converter struct {
-	src io.ReadCloser
+	src   *json.Decoder
+	close func() error
+
+	// buffer idl in case it doesn't fit in p
+	buf      bytes.Buffer
+	decoding decodeTyp
 }
 
-func (c *converter) Read(p []byte) (int, error) {
-	return c.src.Read(p)
+func newConverter(rc io.ReadCloser) (*converter, error) {
+	c := &converter{
+		src:   json.NewDecoder(rc),
+		close: rc.Close,
+	}
+
+	terr := c.init()
+	return c, terr
+}
+
+func (c *converter) init() error {
+	c.src.Token()
+	tok, terr := c.src.Token()
+	if terr != nil {
+		return terr
+	}
+
+	fieldName := tok.(string)
+	if fieldName != "data" {
+		return fmt.Errorf("unexpected field in json: %s", fieldName)
+	}
+	c.src.Token()
+
+	tok, terr = c.src.Token()
+	if terr != nil {
+		return terr
+	}
+
+	fieldName = tok.(string)
+	if fieldName != "__schema" {
+		return fmt.Errorf("expected field: \"__schema\", but got: %s", fieldName)
+	}
+	c.src.Token()
+
+	tok, terr = c.src.Token()
+	if terr != nil {
+		return terr
+	}
+
+	fieldName = tok.(string)
+	switch fieldName {
+	case "directives":
+		c.decoding = decodeDirs
+	case "types":
+		c.decoding = decodeTypes
+	}
+	tok, _ = c.src.Token()
+	return nil
+}
+
+func (c *converter) Read(p []byte) (n int, err error) {
+	if !c.src.More() {
+		t, err := c.src.Token()
+		if err != nil {
+			return 0, err
+		}
+
+		if delim, ok := t.(json.Delim); !ok || delim != ']' {
+			return 0, fmt.Errorf("expected array closing")
+		}
+
+		t, err = c.src.Token()
+		if err != nil {
+			return 0, err
+		}
+		_, ok := t.(json.Delim)
+		if ok {
+			return 0, io.EOF
+		}
+
+		v, ok := t.(string)
+		if !ok {
+			return 0, fmt.Errorf("unexpected token: %v", t)
+		}
+		c.src.Token()
+
+		switch v {
+		case "directives":
+			c.decoding = decodeDirs
+		case "types":
+			c.decoding = decodeTypes
+		}
+
+		return c.Read(p)
+	}
+
+	switch c.decoding {
+	case decodeDirs:
+		d := directive{}
+		err = c.src.Decode(&d)
+		if err != nil {
+			break
+		}
+
+		if d.Description != "" {
+			c.buf.Write([]byte("\""))
+			c.buf.WriteString(d.Description)
+			c.buf.Write([]byte("\"\n"))
+		}
+
+		c.buf.Write([]byte("@"))
+		c.buf.WriteString(d.Name)
+
+		if d.Args != nil {
+			c.buf.Write([]byte("("))
+			writeArgs(&c.buf, d.Args)
+			c.buf.Write([]byte(")"))
+		}
+
+		if d.IsRepeatable {
+			c.buf.Write([]byte(" repeatable"))
+		}
+
+		c.buf.Write([]byte(" on "))
+
+		l := len(d.Locations) - 1
+		for i, loc := range d.Locations {
+			c.buf.WriteString(loc)
+			if i != l {
+				c.buf.Write([]byte(" | "))
+			}
+		}
+	case decodeTypes:
+		t := typ{}
+		err = c.src.Decode(&t)
+		if err != nil {
+			break
+		}
+
+		if t.Description != "" {
+			c.buf.Write([]byte("\""))
+			c.buf.WriteString(t.Description)
+			c.buf.Write([]byte("\"\n"))
+		}
+
+		writeTyp(&c.buf, t)
+	}
+
+	n = copy(p, c.buf.Bytes())
+	c.buf.Truncate(n)
+	return
+}
+
+// TODO
+func writeArgs(w io.Writer, args []*inputValue) {}
+
+const (
+	SCALAR       = "SCALAR"
+	OBJECT       = "OBJECT"
+	INTERFACE    = "INTERFACE"
+	UNION        = "UNION"
+	ENUM         = "ENUM"
+	INPUT_OBJECT = "INPUT_OBJECT"
+	LIST         = "LIST"
+	NON_NULL     = "NON_NULL"
+)
+
+func writeTyp(b *bytes.Buffer, t typ) {
+	switch t.Kind {
+	case SCALAR:
+		b.Write([]byte("scalar "))
+		b.WriteString(t.Name)
+	case OBJECT:
+	case INTERFACE:
+	case UNION:
+	case ENUM:
+	case INPUT_OBJECT:
+	case LIST:
+	case NON_NULL:
+	}
 }
 
 func (c *converter) Close() error {
-	return c.src.Close()
+	return c.close()
 }
