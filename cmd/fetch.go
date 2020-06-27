f02542207d67ad79f90d4d55a74a7e88c0f1b9d8
Zaba505 <cakub6@gmx.com>
2020-06-27 10:45:46 -0400

    fix(fetch): use same introspection query used by graphql-js

--- cmd/fetch.go
+++ cmd/fetch.go
@@ -15,98 +15,94 @@ import (
 	"go.uber.org/zap"
 )
 
-var introQuery = `query {
-	__schema {
-		types {
-			kind
-			name
-			description
+var introQuery = `query IntrospectionQuery {
+      __schema {
+        types {
+          ...FullType
+        }
+        directives {
+          name
+          description
+          locations
+          args {
+            ...InputValue
+          }
+        }
+      }
+    }
+
+    fragment FullType on __Type {
+      kind
+      name
+      description
+      fields(includeDeprecated: true) {
+        name
+        description
+        args {
+          ...InputValue
+        }
+        type {
+          ...TypeRef
+        }
+        isDeprecated
+        deprecationReason
+      }
+      inputFields {
+        ...InputValue
+      }
+      interfaces {
+        ...TypeRef
+      }
+      enumValues(includeDeprecated: true) {
+        name
+        description
+        isDeprecated
+        deprecationReason
+      }
+      possibleTypes {
+        ...TypeRef
+      }
+    }
+
+    fragment InputValue on __InputValue {
+      name
+      description
+      type { ...TypeRef }
+      defaultValue
+    }
+
+    fragment TypeRef on __Type {
+      kind
+      name
       ofType {
         kind
-      }
-
-			fields(includeDeprecated: true) {
-				name
-				description
-				isDeprecated
-				deprecationReason
-				args {
-					name
-					description
-					defaultValue
-					type {
-						kind
-						name
-
+        name
+        ofType {
+          kind
+          name
+          ofType {
+            kind
+            name
             ofType {
               kind
-							name
+              name
+              ofType {
+                kind
+                name
+                ofType {
+                  kind
+                  name
+                  ofType {
+                    kind
+                    name
+                  }
+                }
+              }
             }
-					}
-				}
-				type {
-					kind
-					name
-
-          ofType {
-            kind
-						name
           }
-				}
-			}
-
-			interfaces {
-				name
-			}
-
-			possibleTypes {
-				kind
-				name
-			}
-
-			enumValues(includeDeprecated: true) {
-				name
-				description
-				isDeprecated
-				deprecationReason
-			}
-
-			inputFields {
-				name
-				description
-				defaultValue
-				type {
-					kind
-					name
-
-          ofType {
-            kind
-						name
-          }
-				}
-			}
-		}
-		directives {
-			name
-			description
-			locations
-			args {
-				name
-				description
-				defaultValue
-				type {
-					kind
-					name
-
-          ofType {
-            kind
-						name
-          }
-				}
-			}
-		}
-	}
-}`
+        }
+      }
+    }`
 
 type gqlReq struct {
 	Query string `json:"query"`
